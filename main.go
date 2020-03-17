df5b841b5237226ad3caad7ea48f924b43061118
David Li <davidli012345@gmail.com>
2020-03-17 08:47:54 -0700

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	var svar string
 	flag.StringVar(&svar, "isbn", "9781451648546", "ISBN 13 code search")
 	flag.Parse()
-	bookString := fetch.GetBook("9781451648546")
+	bookString := fetch.GetBookDyn("9781451648546")
 	fmt.Println(bookString)
 }
